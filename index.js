fba1da2b799f0cfe49eb6b61ad5b149c1344f102
radselenium <ajaikrishna.s@radiantglobaltech.com>
2024-10-21 12:51:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type':'text/html'});
-res.end('Hello World!');
-}).listen(8080);
+const http = require('http');
+ 
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Node.js with HTTP server!\n');
+});
+ 
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
